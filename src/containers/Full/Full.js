cd93bb1ae2db29e7711c479af479e4532d6b8b53
Maplezzsd <35908830+Maplezzsd@users.noreply.github.com>
2018-02-12 11:50:55 +0800

    Update Full.js

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -25,18 +25,18 @@ import FontAwesome from '../../views/Icons/FontAwesome/';
 import SimpleLineIcons from '../../views/Icons/SimpleLineIcons/';
 import * as firebase from 'firebase';
 
-// Initialize Firebase
-// TODO: Replace with your project's customized code snippet
 
-var config = {
-    apiKey: "AIzaSyBPweAHcP4em0PYh8XWzEqjNhTk3OBNS5I",
-    authDomain: "dashboard-dev-8b843.firebaseapp.com",
-    databaseURL: "https://dashboard-dev-8b843.firebaseio.com",
-    projectId: "dashboard-dev-8b843",
-    storageBucket: "dashboard-dev-8b843.appspot.com",
-    messagingSenderId: "264557605189"
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDUeEtZZKzFu1wS0n2KvHc7t3Ze7G12gNY",
+    authDomain: "bt3103-a703a.firebaseapp.com",
+    databaseURL: "https://bt3103-a703a.firebaseio.com",
+    projectId: "bt3103-a703a",
+    storageBucket: "bt3103-a703a.appspot.com",
+    messagingSenderId: "482297315123"
   };
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
+
 
 
 class Full extends Component {
